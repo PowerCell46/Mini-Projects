833390659bbaca157b562266a91162e089e38f5b
Peter Gerdzhikov <makotsevo.fan@gmail.com>
2024-01-05 20:27:09 +0200

    implemented all views (at this point completely static)

--- SofiaUniSatelliteExplorer/standaloneExamPrep/djangoWorkingFolder/sofiauniproject/urls.py
+++ SofiaUniSatelliteExplorer/standaloneExamPrep/djangoWorkingFolder/sofiauniproject/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 
+from djangoWorkingFolder.sofiauniproject.views import homeView, createView, mapDetails
+
 urlpatterns = [
-    # path('')
+    path('', homeView, name='home view'),
+    path('create/', createView, name='create view'),
+    path('details/<int:pk>/', mapDetails, name='details view')
 ]
--- SofiaUniSatelliteExplorer/standaloneExamPrep/djangoWorkingFolder/sofiauniproject/views.py
+++ SofiaUniSatelliteExplorer/standaloneExamPrep/djangoWorkingFolder/sofiauniproject/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def homeView(request):
+     return render(request, 'home-page.html')
+
+
+def createView(request):
+    if request.method == "GET":
+        return render(request, 'create-map.html')
+
+
+def mapDetails(request, pk):
+    print(pk)
+    return render(request, 'map-details.html')
