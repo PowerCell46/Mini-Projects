93f8d75e99ec9df8aecd98701f47110982b0af28
Peter Gerdzhikov <makotsevo.fan@gmail.com>
2024-02-04 23:35:18 +0200

    final changes for today

--- GisProject/homePage/main.js
+++ GisProject/homePage/main.js
@@ -4,14 +4,16 @@ function handleSectionDiv(e) {
 
     if (hiddenElement.classList.contains('active')) {
         hiddenElement.classList.remove('active');
-        iconElement.style.transform = 'rotate(0deg)';
+        iconElement.classList.remove("fa-arrow-up");
+        iconElement.classList.add("fa-arrow-down");
         
         setTimeout(() => {
             hiddenElement.style.height = '0';
         }, 1000);
     } else {
         hiddenElement.classList.add('active');
-        hiddenElement.style.height = 'auto'; // Set to your preferred height or 'auto'
-        iconElement.style.transform = 'rotate(180deg)';
+        hiddenElement.style.height = 'auto'; 
+        iconElement.classList.remove("fa-arrow-down");
+        iconElement.classList.add("fa-arrow-up");
     }
 }
